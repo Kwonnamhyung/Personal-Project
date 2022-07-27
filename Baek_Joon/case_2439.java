f7fd2f47a970009047e5931a725d1ac58505b9cb
Kwonnamhyung <rnjsskagud@gmail.com>
2022-07-27 12:43:29 +0900

    Baek_joon_2439

---
+++ Baek_Joon/case_2439.java
@@ -0,0 +1,22 @@
+package Baek_Joon;
+
+import java.util.Scanner;
+
+public class case_2439 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+
+        for(int i = num; i > 0; i--){
+
+            for(int k = 0; k < i; k++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
